04e803882986d5f2624e889e7ac03439a7112599
roman <mromanek1802@gmail.com>
2022-05-28 18:10:29 +0200

    Add responsive layout, fonts, styles

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function checkPassword(){
+    
+    if(passwordRepeatInput.value==passwordInput.value){
+        passwordRepeatInput.classList.remove("error");
+        passwordInput.classList.remove("error");
+        errorText.classList.remove("error");
+
+    }else{
+        passwordRepeatInput.classList.add("error");
+        passwordInput.classList.add("error");
+        errorText.classList.add("error");
+
+    }
+}
+
+let passwordRepeatInput = document.querySelector("#user_password_repeat");
+passwordRepeatInput.addEventListener("input", checkPassword);
+
+let passwordInput = document.querySelector("#user_password");
+passwordInput.addEventListener("input", checkPassword);
+
+let errorText = document.querySelector(".pass");
